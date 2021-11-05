3c42a30be305b7ddf8c9c56aaa745ae13d6baa7a
jacksimpson66 <sc18jegs@leeds.ac.uk>
2021-11-05 23:09:09 +0000

    added cors

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app, catch_all_404s=True)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+CORS(app)
 
 def getApp():
     return app
