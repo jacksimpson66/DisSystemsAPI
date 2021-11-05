8e37345b399a352193d5aebd9a7bc8014a9a6ebb
jacksimpson66 <sc18jegs@leeds.ac.uk>
2021-11-05 21:33:23 +0000

    Heroku

--- app/models.py
+++ app/models.py
@@ -1,37 +1,40 @@
 from app import db
 
 
-#Use flask db migrate to update migration file when changing db
-#use flask db upgrade to make changes
-#flask db downgrade reverts changes back in time
-
+#Module class for db
 class Module(db.Model):
+    #primary key set
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
+    #
     lectures = db.relationship('Lecture', backref="module", lazy="dynamic")
 
     def __repr__(self) -> str:
         return '<Module {}>'.format(self.name)
 
+    #aids with jsonifying data
     def serialize(self):
         return {
             'id': self.id,
             'name': self.name
         }
 
-
+#Lecture class for db
 class Lecture(db.Model):
+    #set primary key
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     room = db.Column(db.String(64))
     day = db.Column(db.String(64))
     lecturer = db.Column(db.String(64))
     duration = db.Column(db.Integer)
+    #foreign key relationship with module, based on module.id
     module_id = db.Column(db.Integer, db.ForeignKey('module.id'))
 
     def __repr__(self) -> str:
         return '<Lecture {}>'.format(self.name)
 
+    #aids in jsonifying data
     def serialize(self):
         return {
             'id': self.id,
--- app/routes.py
+++ app/routes.py
@@ -12,29 +12,30 @@ class ModulesAPI(Resource):
     def get(self):
         #query all modules
         modules = Module.query.all()
-        json_to_return = {
-            'modules': []
-        }
+        json_to_return = []
         #for each module, add it to json_to_return, which is then automatically encoded into JSON by Flask-RESTFUL
         for m in modules:
             serialized_module = Module.serialize(m)
-            json_to_return['modules'].append(serialized_module)
+            json_to_return.append(serialized_module)
         return json_to_return
 
 
 class LecturesAPI(Resource):
     def get(self, module_id):
+        #check to see if module id exists
+        if Lecture.query.filter_by(module_id=module_id).count() < 1:
+            return "Module Does Not Exist",404
+
         #query lectures with module id given by get request
         lectures = Lecture.query.filter_by(module_id=module_id)
-        json_to_return = {
-            'lectures': []
-        }
+        
+        json_to_return = []
         #for each lecture returned from query, add to json structure then return
         for l in lectures:
             serialized_lecture = Lecture.serialize(l)
-            json_to_return['lectures'].append(serialized_lecture)
+            json_to_return.append(serialized_lecture)
         return json_to_return
 
-
+#add endpoints
 api.add_resource(ModulesAPI, '/modules', endpoint='modules')
 api.add_resource(LecturesAPI, '/lectures/<int:module_id>', endpoint='lectures')
